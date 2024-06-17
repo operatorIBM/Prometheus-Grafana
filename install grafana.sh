7330a4e83f962181ea9f1975296cd3e57c6dade6
operatorIBM <119529525+operatorIBM@users.noreply.github.com>
2024-06-17 21:46:48 +0300

    ready file

---
+++ install grafana.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#--------------------------------------------------------------------
+
+# https://grafana.com/grafana/download
+GRAFANA_VERSION="10.4.2"
+PROMETHEUS_URL="http://172.31.29.90:9090"
+
+# install updates 
+apt-get install -y apt-transport-https software-properties-common wget
+mkdir -p /etc/apt/keyrings/
+wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+apt-get update
+apt-get install -y adduser libfontconfig1 musl
+
+# download grafana
+wget https://dl.grafana.com/oss/release/grafana_${GRAFANA_VERSION}_amd64.deb
+dpkg -i grafana_${GRAFANA_VERSION}_amd64.deb
+
+# added PATH for auto to profile 
+echo "export PATH=/usr/share/grafana/bin:$PATH" >> /etc/profile
+
+# parametrs for grafana
+cat <<EOF> /etc/grafana/provisioning/datasources/prometheus.yaml
+apiVersion: 1
+
+datasources:
+  - name: Prometheus
+    type: prometheus
+    url: ${PROMETHEUS_URL}
+EOF
+# reload daemon and start work server
+systemctl daemon-reload
+systemctl enable grafana-server
+systemctl start grafana-server
+systemctl status grafana-server
